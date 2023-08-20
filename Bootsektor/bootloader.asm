876c01bbb1afce7e8814e72eb1a9431c888ca19f
Jens Fischer <JensFZ@gmail.com>
2023-08-20 18:27:18 +0200

    floppy zugriff eingebaut

--- Bootsektor/bootloader.asm
+++ Bootsektor/bootloader.asm
@@ -83,14 +83,132 @@ main:
   mov si, msg_hello
   call puts
 
+  mov [ebr_drive], dl ; dl = drive number
+  mov ax, 1
+  mov cl, 1
+  mov bx, 0x7E00 ; 0x7E00 = 0x7C00 + 512 -> Nach dem Bootsektor
+  call disk_read
+
 
   hlt ;
 
+floppy_error:
+  mov si, floppy_error_msg
+  call puts
+  jmp wait_key_and_reboot
+  hlt
+
+wait_key_and_reboot:
+  mov ah, 0
+  int 16h ; warte auf keypress
+  jmp 0FFFFh:0 ; reboot
+  hlt
+
 .halt: ; HALT
-  jmp .halt
+  cli ; Interrupts deaktivieren
+  hlt ; HALT
+
+
+;
+; Disk Routinen
+;
+
+;
+; Konvertiert LBA zu CHS
+; Params:
+;   - ax: LBA Adresse
+; Returns:
+;   - cx [bits 0-5]: sector
+;   - cx [bits 6-15]: cylinder
+;   - dx: head
+
+lba_to_chs:
+  push ax
+  push dx
+
+  xor dx, dx ; dx = 0
+  div word [dbd_sectors_per_track] ; ax / dbd_sectors_per_track -> dx = Rest, ax = Ergebnis
+  inc dx; dx = dx + 1
+  mov cx, dx ; cx = dx
+
+  xor dx, dx ; dx = 0
+  div word [dbd_heads] ; ax / dbd_heads -> dx = Rest, ax = Ergebnis
+  mov dh, dl ; dh = head
+
+  mov ch, al ; ch = cylinder
+  shl ah, 6 ; ah * 64
+  or cl, ah ; cl = cl + ah
+
+  pop ax
+  mov dl, al ; restore DL
+  pop ax
+  ret
+
+;
+; Liest einen Sektor von der Disk
+; Params:
+;   - ax: LBA sector
+;   - cl: nummer des sektors
+;   - dl: drive
+;   -es:bx: adresse, wo der sektor gespeichert werden soll
+disk_read:
+
+  push ax
+  push bx
+  push cx 
+  push dx
+  push di
+
+  push cx
+  call lba_to_chs
+  pop ax
+
+  mov ah, 02h
+  mov di, 3
+.retry:
+  pusha; alle register auf stack
+  stc ; set carry flag
+  int 13h
+  jnc .success
+
+  popa ; alle register vom stack
+  call disk_reset
+
+  dec di
+  test di, di
+  jnz .retry
+
+.fail
+  jmp floppy_error
+
+.success
+  popa
+
+  pop di
+  pop dx
+  pop cx
+  pop bx
+  pop ax  
+
+  ret
+
+
+;
+; Resetet die Disk
+; Params:
+;   - dl: drive
+disk_reset:
+  pusha
+  mov ah, 0
+  stc
+  int 13h
+  jc floppy_error
+  popa
+  ret
 
 
 msg_hello: db 'Welcome to MyOS', ENDL, 0
+floppy_error_msg: db 'Floppy error', ENDL, 0
 
 times 510-($-$$) db 0 ; 510 - (aktuelle Position - aktuelle sektion) mit 0 auffüllen -> immer 512 bytes 
 
