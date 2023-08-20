cea9053a60eceacebe815ab5e4e8c061144953cf
Jens Fischer <JensFZ@gmail.com>
2023-08-21 00:51:31 +0200

    kleiner testkernel

--- Kernel/main.asm
+++ Kernel/main.asm
@@ -1,14 +1,17 @@
-org 0x7C00 ; 
+org 0x0 ; 
 bits 16 ; 16-Bit design
 
 %define ENDL 0x0D, 0x0A ; Newline + Character Return
 
 start:
-  jmp main
+  mov si, msg_hello
+  call puts
 
-; Schreibt ein String auf den Bildschirm
-; Params:
-;   - ds:si zeigt auf String
+.halt: ; HALT
+  cli
+  hlt ;
+
+
 
 puts:
   push si
@@ -30,31 +33,4 @@ puts:
   pop si
   ret
 
-main:
-
-  ; Setup data
-
-  mov ax, 0    ; es kann nicht direkt in ds und es geschrieben werden
-  mov ds, ax
-  mov es, ax
-
-
-  ;setup stack
-  mov ss, ax
-  mov sp, 0x7C00 ; Stack wächst nach unten -> liegt unterhalb dieses Blocks
-
-  mov si, msg_hello
-  call puts
-
-
-  hlt ;
-
-.halt: ; HALT
-  jmp .halt
-
-
 msg_hello: db 'Welcome to MyOS-Kernel', ENDL, 0
-
-times 510-($-$$) db 0 ; 510 - (aktuelle Position - aktuelle sektion) mit 0 auffüllen -> immer 512 bytes 
-
-dw 0AA55h
