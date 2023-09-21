1b58e50920182b15a4a992b5e0edff7bc7bfeb12
Jens Fischer <JensFZ@gmail.com>
2023-09-21 18:23:49 +0200

    fixes

--- tools/fat/fat.c
+++ tools/fat/fat.c
@@ -11,84 +11,84 @@ typedef uint8_t bool;
 typedef struct
 {
     uint8_t BootJumpInstruction[3]; // 0xEB 0x3C 0x90
-    uint8_t bdb_oem[8]; // OEM name and version
-    uint16_t dbd_bytes; // bytes per sector
-    uint8_t dbd_sectors;    // sectors per cluster
-    uint16_t dbd_reserved;  // reserved sectors
-    uint8_t dbd_fats;    // number of FATs
-    uint16_t dbd_root_entries; // number of root directory entries
-    uint16_t dbd_sectors_small; // number of sectors (small)
-    uint8_t dbd_media; // media descriptor
-    uint16_t dbd_sectors_per_fat; // sectors per FAT
-    uint16_t dbd_sectors_per_track; // sectors per track
-    uint16_t dbd_heads; // number of heads
-    uint32_t dbd_hidden; // hidden sectors
-    uint32_t dbd_sectors_large; // number of sectors (large)
+    uint8_t OemIdentifier[8]; // OEM name and version
+    uint16_t BytesPerSector; // bytes per sector
+    uint8_t SectorsPerCluster;    // sectors per cluster
+    uint16_t ReservedSectors;  // reserved sectors
+    uint8_t FatCount;    // number of FATs
+    uint16_t DirEntryCount; // number of root directory entries
+    uint16_t TotalSectors; // total number of sectors
+    uint8_t MediaDescriptorType; // media descriptor
+    uint16_t SectorsPerFat; // sectors per FAT
+    uint16_t SectorsPerTrack; // sectors per track
+    uint16_t Heads; // number of heads
+    uint32_t HiddenSectors; // hidden sectors
+    uint32_t LargeSectorCount; // number of sectors (large)
 
-    uint8_t ebr_drive; // drive number
-    uint8_t ebr_reserved; // reserved
-    uint8_t ebr_signature; // signature
-    uint32_t ebr_volume_id; // volume ID
-    uint8_t ebr_volume_label[11]; // volume label
-    uint8_t ebr_filesystem[8]; // filesystem type
+    uint8_t DriveNumber; // drive number
+    uint8_t _Reserved; // reserved
+    uint8_t Signature; // signature
+    uint32_t VolumeId; // volume ID
+    uint8_t VolumeLabel[11]; // volume label
+    uint8_t SystemId[8]; // filesystem type
 } __attribute__((packed)) BootSector;
 
 typedef struct {
-    uint8_t name[11]; // file name
-    uint8_t attributes; // file attributes
-    uint8_t reserved;  // reserved
-    uint8_t creation_time_ms; // creation time (milliseconds)
-    uint16_t creation_time; // creation time
-    uint16_t creation_date; // creation date
-    uint16_t last_access_date; // last access date
-    uint16_t first_cluster_high; // first cluster (high word)
-    uint16_t last_modification_time; // last modification time
-    uint16_t last_modification_date; // last modification date
-    uint16_t first_cluster_low; // first cluster (low word)
-    uint32_t file_size; // file size
+    uint8_t Name[11]; // file name
+    uint8_t Attributes; // file attributes
+    uint8_t _Reserved;  // reserved
+    uint8_t CreatedTimeTenths; // creation time (milliseconds)
+    uint16_t CreatedTime; // creation time
+    uint16_t CreatedDate; // creation date
+    uint16_t AccessedDate; // last access date
+    uint16_t FirstClusterHigh; // first cluster (high word)
+    uint16_t ModifiedTime; // last modification time
+    uint16_t ModifiedDate; // last modification date
+    uint16_t FirstClusterLow; // first cluster (low word)
+    uint32_t Size; // file size
 
 } __attribute__((packed)) DirectoryEntry;
 
-BootSector bootSector; // boot sector
-uint8_t* fat1 = NULL; // FAT
-DirectoryEntry* rootDirectory = NULL; // root directory
-uint32_t rootDirectoryEnd; // end of root directory
+BootSector g_BootSector; // boot sector
+uint8_t* g_Fat = NULL; // FAT
+DirectoryEntry* g_RootDirectory = NULL; // root directory
+uint32_t g_RootDirectoryEnd; // end of root directory
 
 bool readBootSector(FILE* disk) { // read boot sector
-    return fread(&bootSector, sizeof(bootSector), 1, disk) > 0; // read boot sector
+    return fread(&g_BootSector, sizeof(g_BootSector), 1, disk) > 0; // read boot sector
 }
 
 
-bool readSectors(FILE* disk, uint32_t lba, uint32_t count, void* buffer) { // read sectors
+bool readSectors(FILE* disk, uint32_t lba, uint32_t count, void* bufferOut) { // read sectors
     bool ok = true;
-    ok = ok && (fseek(disk, lba * bootSector.dbd_bytes, SEEK_SET) == 0); // seek to the beginning of the sector
-    ok = ok && (fread(buffer, bootSector.dbd_bytes, count, disk) == count); // read the sector
+    ok = ok && (fseek(disk, lba * g_BootSector.BytesPerSector, SEEK_SET) == 0); // seek to the beginning of the sector
+    ok = ok && (fread(bufferOut, g_BootSector.BytesPerSector, count, disk) == count); // read the sector
     return ok;
 }
 
 bool readFat(FILE* disk) {
-    fat1 = (uint8_t*)malloc(bootSector.dbd_sectors_per_fat * bootSector.dbd_bytes); // allocate memory for FAT
-    return readSectors(disk, bootSector.dbd_reserved, bootSector.dbd_sectors_per_fat, fat1); // read FAT
+    g_Fat = (uint8_t*)malloc(g_BootSector.SectorsPerFat * g_BootSector.BytesPerSector); // allocate memory for FAT
+    return readSectors(disk, g_BootSector.ReservedSectors, g_BootSector.SectorsPerFat, g_Fat); // read FAT
 }
 
 bool readRootDirectory(FILE* disk) {
-    uint32_t lba = bootSector.dbd_reserved + bootSector.dbd_sectors_per_fat * bootSector.dbd_fats; // calculate LBA of root directory
-    uint32_t size = bootSector.dbd_root_entries * sizeof(DirectoryEntry); // calculate size of root directory
-    uint32_t sectors = (size / bootSector.dbd_bytes); // calculate number of sectors
+    uint32_t lba = g_BootSector.ReservedSectors + g_BootSector.SectorsPerFat * g_BootSector.FatCount; // calculate LBA of root directory
+    uint32_t size = sizeof(DirectoryEntry) * g_BootSector.DirEntryCount; // calculate size of root directory
+    uint32_t sectors = (size / g_BootSector.BytesPerSector); // calculate number of sectors
     
-    if(size % bootSector.dbd_bytes > 0) { // if there is a remainder -> round up
+    if(size % g_BootSector.BytesPerSector > 0) { // if there is a remainder -> round up
         sectors++;
     }
 
-    rootDirectoryEnd = lba + sectors; // calculate end of root directory
-    rootDirectory = (DirectoryEntry*)malloc(sectors * bootSector.dbd_bytes); // allocate memory for root directory
-    return readSectors(disk, lba, sectors, rootDirectory); // read root directory
+    g_RootDirectoryEnd = lba + sectors; // calculate end of root directory
+    g_RootDirectory = (DirectoryEntry*)malloc(sectors * g_BootSector.BytesPerSector); // allocate memory for root directory
+    return readSectors(disk, lba, sectors, g_RootDirectory); // read root directory
 }
 
 DirectoryEntry* findFile(const char* name) {
-    for(uint32_t i = 0; i < bootSector.dbd_root_entries; i++) { // iterate through root directory
-        if(memcmp(name, rootDirectory[i].name, 11) == 0) { // compare file name
-            return &rootDirectory[i]; // return pointer to directory entry
+    for(uint32_t i = 0; i < g_BootSector.DirEntryCount; i++) { // iterate through root directory
+        if(memcmp(name, g_RootDirectory[i].Name, 11) == 0) { // compare file name
+            return &g_RootDirectory[i]; // return pointer to directory entry
         }
     }
 
@@ -97,17 +97,17 @@ DirectoryEntry* findFile(const char* name) {
 
 bool readFile(DirectoryEntry* fileEntry, FILE* disk, uint8_t* outputBuffer) {
     bool ok = true;
-    uint16_t currentCluster = fileEntry->first_cluster_low; // get first cluster of file
+    uint16_t currentCluster = fileEntry->FirstClusterLow; // get first cluster of file
     do {
-        uint32_t lba = rootDirectoryEnd + (currentCluster - 2) * bootSector.dbd_sectors; // calculate LBA of cluster
-        ok = ok && readSectors(disk, lba, bootSector.dbd_sectors_per_fat, outputBuffer); // read cluster
-        outputBuffer += bootSector.dbd_sectors_per_fat * bootSector.dbd_bytes; // advance output buffer
+        uint32_t lba = g_RootDirectoryEnd + (currentCluster - 2) * g_BootSector.SectorsPerCluster; // calculate LBA of cluster
+        ok = ok && readSectors(disk, lba, g_BootSector.SectorsPerCluster, outputBuffer); // read cluster
+        outputBuffer += g_BootSector.SectorsPerCluster * g_BootSector.BytesPerSector; // advance output buffer
 
-        uint32_t fatOffset = currentCluster * 3 / 2; // calculate offset in FAT
+        uint32_t fatIndex  = currentCluster * 3 / 2; // calculate offset in FAT
         if(currentCluster % 2 == 0 ) {
-            currentCluster = (*(uint16_t*)(fat1 + fatOffset)) & 0x0FFF; // read next cluster from FAT;
+            currentCluster = (*(uint16_t*)(g_Fat + fatIndex )) & 0x0FFF; // read next cluster from FAT;
         } else {
-            currentCluster = (*(uint16_t*)(fat1 + fatOffset)) >> 4; // read next cluster from FAT;
+            currentCluster = (*(uint16_t*)(g_Fat + fatIndex )) >> 4; // read next cluster from FAT;
         }
 
     } while (ok && currentCluster < 0x0FF8); // repeat until end of file
@@ -135,14 +135,14 @@ int main( int argc, char** argv) {
 
     if(!readFat(disk)) {
         fprintf(stderr, "Could not read FAT\n");
-        free(fat1);
+        free(g_Fat);
         return -3;
     }
     
     if(!readRootDirectory(disk)) {
         fprintf(stderr, "Could not read root directory\n");
-        free(fat1);
-        free(rootDirectory);
+        free(g_Fat);
+        free(g_RootDirectory);
         return -4;
     }
 
@@ -150,21 +150,21 @@ int main( int argc, char** argv) {
 
     if(!entry) {
         fprintf(stderr, "Could not find file %s\n", argv[2]);
-        free(fat1);
-        free(rootDirectory);
+        free(g_Fat);
+        free(g_RootDirectory);
         return -5;
     }
 
-    uint8_t* outputBuffer = (uint8_t*)malloc(entry->file_size + bootSector.dbd_bytes);
+    uint8_t* outputBuffer = (uint8_t*)malloc(entry->Size + g_BootSector.BytesPerSector);
     if(!readFile(entry, disk, outputBuffer)) {
         fprintf(stderr, "Could not read file %s\n", argv[2]);
-        free(fat1);
-        free(rootDirectory);
+        free(g_Fat);
+        free(g_RootDirectory);
         free(outputBuffer);
         return -6;
     }
 
-    for(size_t i=0; i < entry->file_size; i++) {
+    for(size_t i=0; i < entry->Size; i++) {
         if(isprint(outputBuffer[i])) {
             fputc(outputBuffer[i], stdout);
         } else {
@@ -174,7 +174,7 @@ int main( int argc, char** argv) {
 
     printf("\n");
 
-    free(fat1);
-    free(rootDirectory);
+    free(g_Fat);
+    free(g_RootDirectory);
     return 0;
 }
